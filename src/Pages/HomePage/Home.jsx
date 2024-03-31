ecf5c0e486e5842ab0ed9412bd19d684022b41cc
emmanuel okezie <emmanuelokezie50@gmail.com>
2024-03-31 15:34:44 +0100

    added pages

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -4,6 +4,16 @@ import "./Home.css"
 const Home = () => {
   return (
     <div id="home">
+      <div className="contain">
+        <h1>Getbit</h1>
+        <div className="prod">
+          <div id="b&S">
+            <h1>Buy</h1>
+            <h1>/Sell</h1>
+          </div>
+          <div id="trns">Order</div>
+        </div>
+      </div>
 
     </div>
   )
