119cfa0c84329bf749507a8f07c081251dc0ee21
emmanuel okezie <emmanuelokezie50@gmail.com>
2024-04-05 15:17:28 +0100

    routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import Sigin from './Pages/SignInPage/Sigin'
 import SignUp from './Pages/SignupPage/Signup'
 import Home from './Pages/HomePage/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Buy from './Component/Buy';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Route path="/signin" element={<Sigin />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/*" element={<Home />} /> 
+      <Route path="/buy" element={<Buy />} /> 
     </Routes>
     </BrowserRouter>
     </>
