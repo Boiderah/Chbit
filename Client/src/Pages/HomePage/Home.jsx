4a02abd9d15dc5421b50a8059c2728bbed2568d6
emmanuel okezie <emmanuelokezie50@gmail.com>
2024-04-22 14:56:06 +0100

    home page debug

--- Client/src/Pages/HomePage/Home.jsx
+++ Client/src/Pages/HomePage/Home.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from "react";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
+import btclogo2 from "../../assets/Res/btclogo2.png";
+import footer from "../../assets/Res/footer.png";
+import ethereum from "../../assets/Res/ethereum.png";
+import usdt from "../../assets/Res/usdt.png";
+import litecoin from "../../assets/Res/litecoin.png";
 
 const Home = () => {
   const Navigate = useNavigate();
@@ -30,22 +35,32 @@ const Home = () => {
       <div id="p2pdesignbox">
       <div className="p2pdesignbox1">
         <div className="p2pdesignim1">
-          <img src="" alt="" />
+          <div className="ppplog">
+            <img src={btclogo2} alt="" />
+          </div>
         </div>
-        <div className="p2pdesignim1">
-          <img src="" alt="" />
+        <div className="p2pdesignim2">
+        <div className="ppplog">
+            <img src={ethereum} alt="" />
+          </div>
+        </div>
 
-        </div>
         <div className="p2pdesignbox2">
           <h1> P2P Trading </h1>
           <h1>Safe - Fast - Diverse</h1>
         </div>
-        <div className="p2pdesignim2">
-          <img src="" alt="" />
-          
+        <div className="p2pdesignim1">
+        <div className="ppplog">
+            <img src={usdt} alt="" />
+          </div>
         </div>
         <div className="p2pdesignim2">
-          <img src="" alt="" />
+        <div className="ppplog">
+            <img src={litecoin} alt="" />
+          </div>
+        <div className="ppplog2">
+            <img src={footer} alt="" />
+          </div>
           
         </div>
       </div>
